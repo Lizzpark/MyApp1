c41b3508b1972945aa9c6e36ebfb28817e82915a
lizzpark <1223315068@qq.com>
2016-12-05 17:31:19 +0800

    这是更改过后的 代码

--- app/src/main/java/com/example/administrator/myapp1/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapp1/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.administrator.myapp1;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.tv_main);
+        textView.setText("我是示例文件");
     }
 }
